1167d671f6512839344d1097c637f8ab43751124
Andre Luiz <andre2kluiz@gmail.com>
2025-02-08 15:07:04 -0300

    Criação do DTO e ENTITY

--- src/main/java/br/com/andre/projeto/entity/UsuarioEntity.java
+++ src/main/java/br/com/andre/projeto/entity/UsuarioEntity.java
@@ -1,5 +1,8 @@
 package br.com.andre.projeto.entity;
 
+import org.springframework.beans.BeanUtils;
+
+import br.com.andre.projeto.dto.UsuarioDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -29,6 +32,14 @@ public class UsuarioEntity {
 
     public Long getId() {
         return id;
+    }
+
+    public UsuarioEntity(UsuarioDTO usuario){
+        BeanUtils.copyProperties(usuario, this);
+    }
+
+    public UsuarioEntity(){
+
     }
 
     //Get e Set
@@ -66,6 +77,31 @@ public class UsuarioEntity {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UsuarioEntity other = (UsuarioEntity) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
 }
